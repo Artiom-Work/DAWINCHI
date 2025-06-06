7b07d0cc2434c3fa45b28bf9d6e8191d765e530c
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-06-06 08:54:30 +0300

    added mobile menu and section promo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,43 @@
 'use strict';
+const menuSwitcher = document.getElementById('menu-switch');
+const mobileMenu = document.querySelector('.mobile-menu__wrapper');
+const pageContent = document.querySelector('.content');
+const pageFooter = document.querySelector('.footer');
+// const linkToMobileMenu = document.querySelector('.link-to-mobile-menu'); 
+//for mobile menu
+
+menuSwitcher.addEventListener('change', (e) => {
+	if (e.target.checked) {
+		bodyLock();
+		menuSwitcher.labels[0].title = 'close';
+	} else if (!e.target.checked) {
+		bodyUnlock();
+		menuSwitcher.labels[0].title = 'mobile menu';
+	}
+});
+// linkToMobileMenu.addEventListener('click', (e) => {
+// 	if (mediaQueryMobile.matches) {
+// 		menuSwitcher.checked = true;
+// 		bodyLock();
+// 	}
+// });
+mobileMenu.addEventListener('click', (e) => {
+	menuSwitcher.checked = false;
+	bodyUnlock();
+});
+
+function bodyLock() {
+	// const lockPaddingValue = window.innerWidth - document.querySelector('body').offsetWidth + "px";
+	// document.body.style.paddingRight = lockPaddingValue;
+	document.body.classList.add('lock-body');
+	pageContent.classList.add('hide-layer');
+	pageFooter.classList.add('hide-layer');
+	pageContent.classList.add('blur-layer');
+}
+function bodyUnlock() {
+	// document.body.style.paddingRight = '0px';
+	document.body.classList.remove('lock-body');
+	pageContent.classList.remove('hide-layer');
+	pageFooter.classList.remove('hide-layer');
+	pageContent.classList.remove('blur-layer');
+}

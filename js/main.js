bf2f1a35e856f641ce4263d59f32b205c34b1547
artiom mezheynikov <artiom.mezheynikov.work@gmail.com>
2025-06-11 23:51:32 +0300

    added section start

--- js/main.js
+++ js/main.js
@@ -75,3 +75,24 @@ function bodyUnlock() {
 
 //==========================================================================//
 //==========================================================================//
+
+// For weater widget (hide link)
+// Sourse of widget https://dash.elfsight.com/apps/weather?installationWidgetPid=62b8e7b0-750a-48cf-8d21-dceaadcbf04b
+document.addEventListener('DOMContentLoaded', () => {
+	const weatherWidget = document.querySelector('.elfsight-app-62b8e7b0-750a-48cf-8d21-dceaadcbf04b');
+	if (!weatherWidget) return;
+
+	const observer = new MutationObserver((observer) => {
+		const hideWidgetLink = weatherWidget.querySelector('a[rel="noreferrer"]');
+
+		if (hideWidgetLink) {
+			hideWidgetLink.style.display = 'none';
+		}
+
+		if (hideWidgetLink) {
+			observer.disconnect();
+		}
+	});
+
+	observer.observe(weatherWidget, { childList: true, subtree: true });
+});
